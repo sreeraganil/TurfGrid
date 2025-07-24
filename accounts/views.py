3ea280d3675a79f85dc9c48b7d3aa2f2c82cbadb
sreeraganil <sreeraganil2003@gmail.com>
2025-07-24 11:44:05 +0530

    initial commit

---
+++ accounts/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+def login_view(request):
+    return render(request, 'login.html')
+
+def register_view(request):
+    return render(request, 'register.html')
+
+def landing_view(request):
+    return render(request, 'landing.html')
+
