776b5217ed493ea8ca2d76e63afc95a12e609d02
sreeraganil <sreeraganil2003@gmail.com>
2025-08-01 20:03:00 +0530

    cloudinary

--- main/settings.py
+++ main/settings.py
@@ -118,11 +118,20 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
+USE_CLOUDINARY = config('USE_CLOUDINARY', default=False, cast=bool)
 
-# Media files
+if USE_CLOUDINARY:
+    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+    CLOUDINARY_STORAGE = {
+        'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
+        'API_KEY': config('CLOUDINARY_API_KEY'),
+        'API_SECRET': config('CLOUDINARY_API_SECRET'),
+    }
+else:
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = BASE_DIR / "media"
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / "media" 
+
 
 
 # Default primary key field type
