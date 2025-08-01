afd41baa1777dc2003bd5312ca186a6dc433e11e
sreeraganil <sreeraganil2003@gmail.com>
2025-08-01 20:27:38 +0530

    cloudinary

--- main/settings.py
+++ main/settings.py
@@ -25,6 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary',
+    'cloudinary_storage',
     'accounts',
     'core',
     'turf',
